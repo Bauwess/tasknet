f8c53ac7387e6ed89e8ea8572323be096f9b13a5
Sander <sander@encima.be>
2019-12-10 13:16:17 +0100

    Layouting Open Pane

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="app">
-        <div class="container">
+        <div class="container-fluid pl-5 pr-5">
             <div class="row">
-                <div class="col-sm">
-                    <ul class="task-list">
-                        <li class="task" v-for="task in tasks" :key="task.id" @click="activateTask(task.id)">{{ task.title }}</li>
-                    </ul>
+                <div class="col-sm task-list-column">
+                    <transition-group tag="ul" name="slide-fade-list" appear class="task-list">
+                        <li class="task" :class="{ active : isTaskActive(task.id) }" v-for="task in tasks" :key="task.id">
+                            <div class="remove-task"><span @click="removeTask(task.id)">[X]</span></div>
+                            <div class="task-title" @click="activateTask(task)">{{ task.title }}</div>
+                        </li>
+                    </transition-group>
                 </div>
                 <transition name="slide-fade">
-                    <TaskDetail :task="activeTask" />
+                    <div class="col-sm task-detail-pane" v-if="paneOpen">
+                        <span class="pull-right" @click="closePane">X</span>
+                        <TaskDetail :task="activeTask"/>
+                    </div>
                 </transition>
             </div>
         </div>
@@ -19,7 +25,7 @@
 <script>
 
     import axios from 'axios'
-    import TaskDetail from "./components/TaskDetail";
+    import TaskDetail from "./components/TaskDetail.vue";
 
     export default {
         name: 'app',
@@ -27,15 +33,34 @@
         data() {
             return {
                 activeTask: null,
+                paneOpen: false,
                 tasks: null,
                 errors: []
             };
         },
         methods: {
-            activateTask: function(id) {
-                this.activeTask = this.tasks.find(t => {
-                    return t.id === id
-                })
+            openPane: function(){
+                this.paneOpen = true;
+            },
+            closePane: function(){
+                this.paneOpen = false;
+            },
+            activateTask: function(task) {
+                this.activeTask = task;
+                this.openPane();
+            },
+            removeTask: function(id){
+                let index = this.tasks.findIndex(task => task.id === id);
+                this.tasks.splice(index, 1);
+                if(this.activeTask.id === id){
+                    this.closePane();
+                }
+            },
+            isTaskActive: function(id){
+                if(this.activeTask !== null){
+                    return this.activeTask.id === id
+                }
+                return false
             },
             getTaskList: function () {
                 axios.get("https://jsonplaceholder.typicode.com/todos/")
@@ -45,7 +70,7 @@
                     .catch(e => {
                         this.errors.push(e)
                     })
-            }
+            },
         },
         mounted() {
             this.getTaskList()
@@ -60,41 +85,97 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        margin-top: 50px;
+    }
+
+    .task-list-column {
+        height: 90vh;
+        overflow-y: scroll;
+        background-color: #ffffff;
+        border: 1px solid #ececec;
+        margin-right: 10px;
+
+    }
+
+    .task-detail-pane {
+        background-color: #ffffff;
+        border: 1px solid #ececec;
     }
 
     .task-list {
         padding-inline-start: 0;
     }
 
+    .task-list .task .remove-task {
+        width: 40px;
+        display: inline-block;
+    }
+
+    .task-list .task .remove-task:hover {
+        color: darkred;
+        font-weight: bold;
+    }
+
+    .task-list .task .task-title {
+        width: calc(100% - 40px);
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
     .task-list .task {
+        display: flex;
+        align-items: center;
+        font-size: 13px;
         text-align: left;
-        padding: 7px;
-        border: 1px solid #ececec;
+        padding: 0 7px;
+        height: 36px;
+        border-bottom: 1px solid #ececec;
         margin-bottom: -1px;
         list-style: none;
+        cursor: pointer;
+    }
+
+    .task-list .task.active {
+        background-color: #edf8ff;
     }
 
     .slide-fade-enter {
         transform: translateX(500px);
-        /*opacity: 0;*/
-        background-color: red;
+        opacity: 0;
     }
 
     .slide-fade-enter-active {
         transition: all 0.5s ease;
-        background-color: red;
     }
 
     .slide-fade-leave-active {
         transition: all 0.5s ease;
-        background-color: darkblue;
     }
 
     .slide-fade-leave-to {
         transform: translateX(100%);
-        /*opacity: 0;*/
-        background-color: darkblue;
+        opacity: 0;
+    }
+
+    .slide-fade-list-enter {
+        transform: translateX(30px);
+        opacity: 0;
+    }
+
+    .slide-fade-list-enter-active {
+        transition: all 0.2s ease;
+    }
+
+    .slide-fade-list-leave-active {
+        transition: all 0.5s ease;
+        background-color: darkred!important;
+        border-bottom: 1px solid #ececec;
+    }
+
+    .slide-fade-list-leave-to {
+        transform: translateX(-100%);
+        border-bottom: 1px solid #ececec;
+        opacity: 0;
     }
 
 </style>
--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,59 +1,44 @@
 <template>
-  <div class="col-sm" v-if="task">
-    <span class="pull-right" @click="closeTask">X</span>
-    {{ task.id }} {{ task.title }}
-  </div>
+    <div class="task-detail" v-if="task">
+        {{ task.id }} {{ task.title }}
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'TaskDetail',
-  props: ['task'],
-  data() {
-    return {
-      activeTask: this.task
-    };
-  },
-  methods: {
-    closeTask: function() {
-      this.task = null
+
+    export default {
+        name: 'TaskDetail',
+        props: ['task'],
+        data() {
+            return {
+                activeTask: this.task
+            };
+        },
+        methods: {
+
+        }
     }
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
 
-.slide-fade-enter {
-  transform: translateX(30px);
-  opacity: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-.slide-fade-enter-active {
-  transition: all 0.2s ease;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-.slide-fade-leave-active {
-  transition: all 0.2s ease;
-}
+    a {
+        color: #42b983;
+    }
 
-.slide-fade-leave-to {
-  transform: translateX(-30px);
-  opacity: 0;
-}
 </style>
